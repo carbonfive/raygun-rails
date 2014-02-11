6c7112aa1033feba2aec5057657fa66c48ac4ce5
Christian Nelson <christian@carbonfive.com>
2014-02-11 08:00:12 -0800

    Sort rspec helpers before requiring them for consistency across filesystems.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,7 +34,6 @@ require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 require 'capybara/poltergeist'
 
@@ -44,6 +43,7 @@ end
 
 Capybara.default_driver    = :rack_test
 Capybara.javascript_driver = :poltergeist
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
