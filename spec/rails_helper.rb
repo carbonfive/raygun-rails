fb43ac71067b2e5d887f2facfbcca623c10038fe
Christian Nelson <christian@carbonfive.com>
2015-06-23 18:12:23 -0700

    Update rspec config to match defaults for latest version.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
-require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
+
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require 'spec_helper'
 require 'rspec/rails'
+# Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
