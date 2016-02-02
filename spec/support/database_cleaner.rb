016be8132c25b23767dd683c642e18988429c255
Alex Daniel <adaniel@carbonfive.com>
2016-02-02 15:20:59 -0800

    Use truncation strategy for all features, not just js specs

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -10,7 +10,7 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, js: true) do
+  config.before(:each, type: :feature) do
     DatabaseCleaner.strategy = :truncation
   end
 
