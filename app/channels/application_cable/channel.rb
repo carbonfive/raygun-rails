3599b9753e12b9cd64e2db8609fd0d1a6c90dae0
Christian Nelson <christian@carbonfive.com>
2016-05-13 09:28:42 -0700

    Comment out ActionCable items.

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
-module ApplicationCable
-  class Channel < ActionCable::Channel::Base
-  end
-end
+# module ApplicationCable
+#   class Channel < ActionCable::Channel::Base
+#   end
+# end
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
-module ApplicationCable
-  class Connection < ActionCable::Connection::Base
-  end
-end
+# module ApplicationCable
+#   class Connection < ActionCable::Connection::Base
+#   end
+# end
