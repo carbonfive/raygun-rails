99c09179803b63e7746cadefb2cb943224970c39
Christian Nelson <christian@carbonfive.com>
2017-02-28 15:07:15 -0800

    Config file tweaks for 5.1.0.beta1.

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 # Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers a minimum and maximum.
+# The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum, this matches the default thread size of Active Record.
+# and maximum; this matches the default thread size of Active Record.
 #
 threads_count = ENV.fetch('PUMA_THREADS') { 5 }.to_i
 threads threads_count, threads_count
