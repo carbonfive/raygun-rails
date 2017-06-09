2f8ffda6edc5a45027a88034bc5025049f07d033
Christian Nelson <christian@carbonfive.com>
2017-06-09 10:25:42 -0700

    No longer needed

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,5 +1,4 @@
 # Use this setup block to configure all options available in SimpleForm.
-# rubocop:disable Metrics/BlockLength:
 SimpleForm.setup do |config|
   config.error_notification_class = "alert alert-danger"
   config.button_class = "btn btn-default"
