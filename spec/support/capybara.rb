192e002684373137b7cbe8884c2ac249216a123b
Christian Nelson <christian@carbonfive.com>
2017-10-27 07:35:37 -0700

    Add comment about --no-sandbox

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,6 +3,11 @@ require "selenium/webdriver"
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument("--headless")
+
+  # Try this if chrome crashes.
+  # https://sites.google.com/a/chromium.org/chromedriver/help/chrome-doesn-t-start
+  # options.add_argument("--no-sandbox")
+
   options.add_argument("--window-size=1280,1024")
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
