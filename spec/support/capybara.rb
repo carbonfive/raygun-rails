b37bfcbf1b47689bf0da3b01e291d78408980198
Christian Nelson <christian@carbonfive.com>
2017-10-26 21:39:03 -0700

    Tidy up chrome capybara config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,12 +1,10 @@
 require "selenium/webdriver"
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
 Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: { args: %w[headless] })
-  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless")
+  options.add_argument("--window-size=1280,1024")
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.javascript_driver = :headless_chrome
