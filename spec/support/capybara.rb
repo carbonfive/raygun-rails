cabd73d02886eb579c20692c98db75ca31c326eb
Christian Nelson <christian@carbonfive.com>
2017-10-27 09:35:33 -0700

    Dashes are not necessary

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,12 +2,12 @@ require "selenium/webdriver"
 
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument("--headless")
-  options.add_argument("--window-size=1280,2000")
+  options.add_argument("headless")
+  options.add_argument("window-size=1280,2000")
 
   # Try this if chrome crashes.
   # https://sites.google.com/a/chromium.org/chromedriver/help/chrome-doesn-t-start
-  # options.add_argument("--no-sandbox")
+  # options.add_argument("no-sandbox")
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
