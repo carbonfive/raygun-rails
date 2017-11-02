768ada90d538b4017deb35dde385409505241985
Christian Nelson <christian@carbonfive.com>
2017-11-02 09:17:05 -0700

    Include a comment about the logging prefs

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,13 +3,20 @@ require "selenium/webdriver"
 Capybara.register_driver :headless_chrome do |app|
   options = { "args" => %w[headless window-size=1024,3840] }
 
-  # Try this if chrome crashes.
-  # https://sites.google.com/a/chromium.org/chromedriver/help/chrome-doesn-t-start
+  # Try this if chrome crashes. https://sites.google.com/a/chromium.org/chromedriver/help/chrome-doesn-t-start
   # options["args"] << "no-sandbox"
+
+  # This enables access to the JS console logs in your feature specs.
+  # You can see the logs during the test by calling (for example):
+  #
+  #   puts page.driver.browser.manage.logs.get(:browser).map(&:inspect).join("\n")
+  #
+  # This will print out each log entry in the JS log, including e.g. the React welcome notice.
+  logging_prefs = { "browser" => "ALL" }
 
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     chrome_options: options,
-    logging_prefs: { "browser" => "ALL" }
+    logging_prefs: logging_prefs
   )
 
   Capybara::Selenium::Driver.new(
