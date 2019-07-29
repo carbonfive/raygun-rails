b4caa09adc33883ab41b90c9a9fd93376ea1897f
Christian Nelson <christian@carbonfive.com>
2019-07-29 15:11:53 -0700

    Make chrome headless again
    Headless mode stopped working for some reason sometime over the last couple of months. Tweaking how we tell chrome to be headless appears to make it work again.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,10 +7,12 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara.register_driver :headless_chrome do |app|
-  options = { "args" => %w[headless window-size=1024,3840] }
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  browser_options.args << "--headless"
+  browser_options.args << "--window-size=1024,3840"
 
   # Try this if chrome crashes. https://sites.google.com/a/chromium.org/chromedriver/help/chrome-doesn-t-start
-  # options["args"] << "no-sandbox"
+  # browser_options.args << "--no-sandbox"
 
   # This enables access to the JS console logs in your feature specs.
   # You can see the logs during the test by calling (for example):
@@ -18,16 +20,14 @@ Capybara.register_driver :headless_chrome do |app|
   #   puts page.driver.browser.manage.logs.get(:browser).map(&:inspect).join("\n")
   #
   # This will print out each log entry in the JS log, including e.g. the React welcome notice.
-  logging_prefs = { "browser" => "ALL" }
-
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chrome_options: options,
-    logging_prefs: logging_prefs
+    logging_prefs: { "browser" => "ALL" }
   )
 
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
+    options: browser_options,
     desired_capabilities: capabilities
   )
 end
