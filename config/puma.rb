896e13ce666b5c068c9f624e7c225f6700da57f1
Christian Nelson <christian@carbonfive.com>
2019-08-16 15:07:57 -0700

    Comment consistency nit-pick.

--- config/puma.rb
+++ config/puma.rb
@@ -17,6 +17,7 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
+#
 pidfile     ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Specifies the number of `workers` to boot in clustered mode.
