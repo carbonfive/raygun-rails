b5830108e6b58e8fbc5f029b1a7e299ff13c7499
Eric Fung <ericf@carbonfive.com>
2019-10-04 09:25:27 -0700

    Include changes in a new app configured with `rails new --webpacker`

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,11 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
