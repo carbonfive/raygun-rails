bdb158a82a8761046f992212d662bd4b50bc3654
Eric Fung <eskfung@users.noreply.github.com>
2019-10-14 17:16:50 -0700

    Disable asset pipeline completely (#267)
    * Disable sprockets and traditional asset pipeline
    
    * Have webpacker handle global styles
    
    * Add direct variable imports to satisfy webpack-dev-server, make
    dependencies explicit
    
    * Reset assets pipeline to defaults
    
    * Clear assets completely
    
    * Use consistent semicolons
    
    Until a linter is added

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require "active_model/railtie"
 require "active_record/railtie"
 require "active_storage/engine"
 # require "rails/test_unit/railtie"
-require "sprockets/railtie"
+# require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
