8dec6792f6a5c77c5ba2f8adb8c590520d3f7607
Jon Rogers <jon@carbonfive.com>
2019-12-16 16:32:45 -0800

    Address PR comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,12 +8,4 @@ module ApplicationHelper
         "Update"
     end
   end
-
-  def alert_class(alert_type)
-    alert_type = {
-      alert: "danger",
-      notice: "info"
-    }.with_indifferent_access.fetch(alert_type, alert_type.to_s)
-    "alert-#{alert_type}"
-  end
 end
