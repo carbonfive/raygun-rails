712413e15a375f87933aafd4a68a431f11909887
Matt Brictson <mattb@carbonfive.com>
2021-09-29 09:57:38 -0700

    Send `cache-control:no-store` header by default
    When building apps and APIs, we typically want clients to have the most
    up to date data. If browsers cache highly dynamic responses, this can
    lead to inconsistencies for the user and other odd behavior.
    
    By default, Rails adds this header to all responses:
    
    ```
    Cache-Control: max-age=0, private, must-revalidate
    ```
    
    However, this is not sufficient to prevent caching. The `private`
    directive means the browser is still allow to maintain a private cache
    of the response at its own discretion.
    
    Fix this by sending this header instead:
    
    ```
    Cache-Control: no-store
    ```
    
    This disables all browser caching.
    
    Note that there may still be cases where caching is desirable, e.g. to
    improve performance or where the resource is mostly static. Although the
    default is now `no-store`, you can still customize the cache headers per
    controller/action by using something like `fresh_when`.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  after_action :cache_control_no_store_by_default
+
+  private
+
+  def cache_control_no_store_by_default
+    response.cache_control[:no_store] = true if response.cache_control.empty?
+  end
 end
