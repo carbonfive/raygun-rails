8f5d5904c89bdd3daa900a71ec8aa88b09a988f3
Matt Brictson <mattb@carbonfive.com>
2021-10-18 17:50:21 -0700

    Upgrade to selenium-webdriver 4.0.0
    The new version of selenium-webdriver has some breaking changes to its
    configuration API. This commit upgrades to the latest version of the
    selenium-related gems and makes the necessary changes to fix deprecation
    warnings and errors relating to the new APIs.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,28 +7,25 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara.register_driver :headless_chrome do |app|
-  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new(
+    # This enables access to the JS console logs in your feature specs.
+    # You can see the logs during the test by calling (for example):
+    #
+    #   puts page.driver.browser.manage.logs.get(:browser).map(&:inspect).join("\n")
+    #
+    # This will print out each log entry in the JS log, including e.g. the React welcome notice.
+    logging_prefs: { "browser" => "ALL" }
+  )
   browser_options.args << "--headless"
   browser_options.args << "--window-size=1024,3840"
 
   # Try this if chrome crashes. https://sites.google.com/a/chromium.org/chromedriver/help/chrome-doesn-t-start
   # browser_options.args << "--no-sandbox"
 
-  # This enables access to the JS console logs in your feature specs.
-  # You can see the logs during the test by calling (for example):
-  #
-  #   puts page.driver.browser.manage.logs.get(:browser).map(&:inspect).join("\n")
-  #
-  # This will print out each log entry in the JS log, including e.g. the React welcome notice.
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    logging_prefs: { "browser" => "ALL" }
-  )
-
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
-    options: browser_options,
-    desired_capabilities: capabilities
+    capabilities: [::Selenium::WebDriver::Remote::Capabilities.chrome, browser_options]
   )
 end
 
